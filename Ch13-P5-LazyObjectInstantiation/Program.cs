6366fde209e83005acb1d2d665d9540b8ad7fb5f
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-09 16:55:32 +0500

    Project Created

---
+++ Ch13-P5-LazyObjectInstantiation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch13_P5_LazyObjectInstantiation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
